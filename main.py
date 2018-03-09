68b9dc17005ca7e13fa0903b8c2261576f2a1e06
gabefreedman <32077552+gabefreedman@users.noreply.github.com>
2018-03-09 09:13:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,57 +5,10 @@ Created on Mon Mar  5 17:31:34 2018
 @author: Gabe Freedman
 """
 
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import time
+import lyrics
+import vocab-scrape
 
-#def get_unique_discography():
-BASE_URL = 'http://www.metrolyrics.com/radiohead-albums-list.html'
-page = requests.get(BASE_URL)
-soup = BeautifulSoup(page.text, 'html.parser')
+artist = 'Radiohead'.lower()
 
-num_pages = soup.find('span', class_='pages').get_text().replace('\n', 
-                     '').replace('\t', '')[-1]
-
-
-song_list = []
-heading_tags = []
-albums = []
-page_num = 1
-df = pd.DataFrame(columns=['Artist', 'Album', 'Track'])
-
-while page_num <= int(num_pages):
-    for tag in soup.find_all('div', class_='content song-list compact clearfix'):
-        for link in tag.find_all_next('div', class_='metacritic'):
-            link.decompose()
-        header = tag.find_previous_sibling('header')
-        heading_tags.append(header)
-    
-    for tag in heading_tags:
-        song_table = tag.find_next_sibling('div', class_='content song-list compact clearfix')
-        song_list.append(song_table.text.replace('\n', '').replace(
-                 'Lyrics', 'Lyrics '))
-    
-    for header in heading_tags:
-        album = header.find('span')
-        albums.append(album.text)
-    
-    
-    for i in range(len(albums)):
-        albums[i] = albums[i].strip()
-        for song in song_list[i].split(' Lyrics '):
-            song = song.strip()
-            df = df.append({'Artist':'Radiohead', 'Album':albums[i], 'Track':song}, 
-                            ignore_index=True)
-    
-    page_num += 1
-    time.sleep(2)
-    
-    BASE_URL = 'http://www.metrolyrics.com/radiohead-albums-list-' + str(page_num) + '.html'
-    page = requests.get(BASE_URL)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    song_list = []
-    heading_tags = []
-    albums = []
-
+disc = lyrics.get_unique_discography(artist)
+disc = lyrics.clean_discography(disc)
